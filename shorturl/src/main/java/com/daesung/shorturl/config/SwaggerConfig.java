1834af36c12177cd3fdb35e04d706812ab02b645
LAPTOP-GFIEF03U\fbeot <fbeotjd.unimewo@gmail.com>
2021-03-15 16:41:22 +0900

    swagger 적용 (아직 테스트 x )

---
+++ shorturl/src/main/java/com/daesung/shorturl/config/SwaggerConfig.java
@@ -0,0 +1,58 @@
+package com.daesung.shorturl.config;
+
+import java.util.HashSet;
+import java.util.Set;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ *
+ * @author MW
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket customImplementation() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .consumes(getConsumeContentTypes())
+                .produces(getProduceContentTypes())
+                .apiInfo(getApiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private Set<String> getConsumeContentTypes() {
+        Set<String> consumes = new HashSet<>();
+        consumes.add("application/json;charset=UTF-8");
+        consumes.add("application/x-www-form-urlencoded");
+        return consumes;
+    }
+
+    private Set<String> getProduceContentTypes() {
+        Set<String> produces = new HashSet<>();
+        produces.add("application/json;charset=UTF-8");
+        return produces;
+    }
+
+    private ApiInfo getApiInfo() {
+        return new ApiInfoBuilder()
+                .title("urlShorten")
+                .description("[url] API")
+                .contact(new Contact("urlShorten", "https://github.com/yskkkkkk/urlShorten/tree/main/shorturl", "fb1014@naver.com"))
+                .version("1.0")
+                .build();
+    }
+}
--- shorturl/src/main/java/com/daesung/shorturl/feature/url/UrlController.java
+++ shorturl/src/main/java/com/daesung/shorturl/feature/url/UrlController.java
@@ -1,25 +1,27 @@
 package com.daesung.shorturl.feature.url;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import com.daesung.shorturl.model.dto.UrlDTO;
 
+import io.swagger.annotations.*;
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("url")
 @RequiredArgsConstructor
+@Api(value = "SwaggerTestController")
 public class UrlController {
 
 	private final UrlService urlService;
 	
+	@ApiOperation(value = "insOriginUrl", notes = "입력")
 	@PostMapping("url")
 	public String insOriginUrl(@RequestBody UrlDTO urlDTO){
 		return urlService.insOriginUrl(urlDTO);
 	}
 	
+	@ApiOperation(value = "getUrlInfo", notes = "입력")
 	@GetMapping("url")
 	public String getUrlInfo(@RequestParam String urlKey){
 		UrlDTO urlDTO = new UrlDTO();
