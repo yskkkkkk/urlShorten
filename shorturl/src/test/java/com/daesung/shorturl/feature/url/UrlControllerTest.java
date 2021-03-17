61383c4c600e536f42e1ea85a075967127e529a7
LAPTOP-GFIEF03U\fbeot <fbeotjd.unimewo@gmail.com>
2021-03-17 22:31:19 +0900

    03/17 test 방법 변경, 폴더명 변경 의존성 추가

--- shorturl/src/test/java/com/daesung/shorturl/feature/url/UrlControllerTest.java
+++ shorturl/src/test/java/com/daesung/shorturl/feature/url/UrlControllerTest.java
@@ -1,56 +1,30 @@
 package com.daesung.shorturl.feature.url;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.daesung.shorturl.model.dto.UrlDTO;
 
 
-@RunWith(SpringRunner.class)
-@WebMvcTest(UrlController.class)
-@WebAppConfiguration
+@SpringBootTest
 class UrlControllerTest {
 
 	private static final Logger logger = LoggerFactory.getLogger(UrlControllerTest.class);
 
 	@Autowired
-	private WebApplicationContext wac;
-	@Autowired
-	private MockMvc mock;
-
-	@MockBean
 	private UrlService urlService;
 
+	
 	@Before
 	public void beforeTest() {
 		logger.info("===== beforeTest() =====");
-		mock = MockMvcBuilders.webAppContextSetup(wac).build();
-		logger.info("wac: " + wac);
-		logger.info("mock: " + mock);
-	}
-
-	@Test
-	public void testTest() throws Exception {
-		logger.info("===== testTest() =====");
-
-		mock.perform(get("/url/hello")).andExpect(status().isOk()).andExpect(content().string("hello"));
 	}
 
 	@After
@@ -60,21 +34,14 @@ class UrlControllerTest {
 
 	@Test
 	void testInsOriginUrl() throws Exception {
-		
-		mock.perform(post("/url/url")
-				.content("{\"urlKey\": \"https://knowing-today.blogspot.com/\"}")
-				.contentType(MediaType.APPLICATION_JSON)
-				.accept(MediaType.APPLICATION_JSON))
-				.andDo(print())
-				.andExpect(status().isOk());
+		UrlDTO urlDTO = new UrlDTO("http://www.kakao.com");
+		assertThat(urlService.insOriginUrl(urlDTO)).isNotNull();
 	}
-
 	@Test
 	void testGetUrlInfo() throws Exception {
-		mock.perform(get("/url/url")
-				.param("urlKey","cc"))
-				.andDo(print())
-				.andExpect(status().isOk());
+		UrlDTO urlDTO = new UrlDTO();
+		urlDTO.setUrl_key("cc");
+		assertThat(urlService.getUrlInfo(urlDTO)).isEqualTo("https://www.naver.com");
 	}
 
 }
